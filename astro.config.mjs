6700330b4dbd29fc2fbad95f4a011a715b26a076
Morritz <12800230+Morritz@users.noreply.github.com>
2022-01-05 23:01:51 +0100

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
+  // Comment out "renderers: []" to enable Astro's default component support.
+  renderers: [],
+  devOptions: {
+    tailwindConfig: "./tailwind.config.js",
+  },
 });
